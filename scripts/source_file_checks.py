471558d4291bd3de0f6e2170ef117158df1f5d03
Alex Turner <alex@turntrout.com>
2025-07-23 08:16:07 -0700

    chore: linter fixes

--- scripts/source_file_checks.py
+++ scripts/source_file_checks.py
@@ -286,7 +286,11 @@ def check_card_image(metadata: dict) -> List[str]:
 
     try:
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/58.0.3029.110 Safari/537.36"
+            )
         }
         response = requests.head(card_image_url, timeout=10, headers=headers)
         if not response.ok:
