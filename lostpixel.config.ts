a0144c2cc0e383209d0c1f6285ec368df78e40fa
Alex Turner <alex@turntrout.com>
2025-08-15 11:10:51 -0700

    test: attempting to have 0-threshold lostpixel

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -6,5 +6,5 @@ export const config: CustomProjectConfig = {
   },
   lostPixelProjectId: process.env.LOST_PIXEL_PROJECT_ID,
   apiKey: process.env.LOST_PIXEL_API_KEY,
-  threshold: 0.008,
+  threshold: 0,
 }
