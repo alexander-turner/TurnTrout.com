11834c1df1165a4bf3bbfc42d3f7ef1a5ca781b7
Alex Turner <alex@turntrout.com>
2025-09-05 11:55:37 -0700

    Revert "fix: using history.scrollRestoration instead of window (incorrect API call)"
    This reverts commit d417399ec9ce1cff83746081ae9f92b9b1747752.

--- quartz/plugins/emitters/componentResources.ts
+++ quartz/plugins/emitters/componentResources.ts
@@ -102,7 +102,7 @@ function addGlobalPageResources(ctx: BuildCtx, componentResources: ComponentReso
 
   // Early scroll restoration to prevent flicker
   componentResources.beforeDOMLoaded.push(`
-    history.scrollRestoration = "manual"
+    window.scrollRestoration = "manual"
     console.debug("Manual scroll restoration enabled.")
   `)
 
