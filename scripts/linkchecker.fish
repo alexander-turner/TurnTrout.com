d65aa057b2be4863a792427ebf5c1c6c89424744
Alex Turner <alex@turntrout.com>
2025-11-02 07:58:58 -0800

    fix: ignoring about: in linkchecker

--- scripts/linkchecker.fish
+++ scripts/linkchecker.fish
@@ -1,28 +1,24 @@
 #!/usr/bin/env fish
 
-# If there are no arguments passed, then default to GIT_ROOT's public directory
-set -l GIT_ROOT (git rev-parse --show-toplevel)
-
-# Target files for the html linkcheckers
+# If there are no arguments passed, use the live server to resolve relative links
+# Otherwise use the provided files
 set -l TARGET_FILES $argv
 if test -z "$TARGET_FILES"
-    set TARGET_FILES $GIT_ROOT/public/**html
+    set TARGET_FILES "http://localhost:8080"
+    set -x no_proxy "http://localhost:8080"
 end
+
+# Ignore about: URLs
+set -l ABOUT_IGNORE_PATTERNS --ignore-url="about:.*" --ignore-url="https?://about:.*"
 
 # Internal links should NEVER 404! Check links which start with a dot or slash
-# Use the live server to resolve relative links
-if test -z $argv
-    set -x no_proxy "http://localhost:8080"
-    linkchecker http://localhost:8080 --threads 50 
-else
-    linkchecker $TARGET_FILES --threads 50 
-end
-
+linkchecker $TARGET_FILES --threads 50 $ABOUT_IGNORE_PATTERNS
 set -l INTERNAL_STATUS $status
 
 # Check external links which I control
 linkchecker $TARGET_FILES \
     --ignore-url="!^https://(assets\.turntrout\.com|github\.com/alexander-turner/TurnTrout\.com)" \
+    $ABOUT_IGNORE_PATTERNS \
     --no-warnings \
     --check-extern \
     --threads 30 \
